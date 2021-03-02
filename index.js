adc4bd67f742843c5744936ba5b6d3e6e99c091f
Kevin Sajan <kevsaj@gmail.com>
2021-03-01 23:53:58 -0500

    navigate to birthdays page

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
+require('dotenv').config({
+    path: './.env'
+})
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto('https://facebook.com');
-  // await page.screenshot({ path: 'example.png' });
-  await browser.waitForTarget(() => false);
-  await browser.close();
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 50, // slow down by 250ms
+        defaultViewport: null,
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.facebook.com', {
+        waitUntil: 'networkidle2'
+    });
+    // await page.screenshot({ path: 'example.png' });
+
+    await page.type('#email', process.env.USER);
+    await page.type('#pass', process.env.PASS);
+    await page.keyboard.press('Enter');
+
+    await page.waitForNavigation({
+        waitUntil: 'networkidle0'
+    });
+    await page.waitForTimeout(1000);
+
+    // await page.keyboard.press('/');
+    // await page.type('input[type="search"]', 'birthdays');
+    // await page.keyboard.press('Enter');
+
+    await page.goto('https://www.facebook.com/events/birthdays', {
+        waitUntil: 'networkidle2'
+    });
+
+    // await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    await page.waitForTimeout(500);
+
+    await page.keyboard.press('Enter');
+
+    // const element = await select(page).getElement(`span:contains(Today's birthdays)`);
+    // await element.click();
+
+    await page.mouse.click(363, 59);
+
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.type('Happy Birthday!!');
+
+
+    // await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    // await page.waitForTimeout(5000);
+
+    await browser.waitForTarget(() => false);
+    await browser.close();
 })();
